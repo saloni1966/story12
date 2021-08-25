0793e5a22b0b0b8662fe81d369fdd579855e7d41
saloni <salonisharma1966@gmail.com>
2021-08-25 05:47:29 +0530

    2nd commit

--- story.js
+++ story.js
@@ -3,3 +3,9 @@ A timid little Mouse came upon him unexpectedly, and in her fright and haste to
 get away, ran across the Lion's nose. Roused from his nap, the Lion laid his 
 huge paw angrily on the tiny creature to kill her.
 
+
+"Spare me!" begged the poor Mouse. 
+"Please let me go and some day I will surely repay you."
+
+The Lion was much amused to think that a Mouse could ever help him.
+ But he was generous and finally let the Mouse go.
