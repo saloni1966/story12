b4a149d29e91b3a2f6c27f28cf2b4c08d0e6cef1
saloni <salonisharma1966@gmail.com>
2021-08-25 05:45:37 +0530

    1st commit

---
+++ story.js
@@ -0,0 +1,5 @@
+A Lion lay asleep in the forest, his great head resting on his paws. 
+A timid little Mouse came upon him unexpectedly, and in her fright and haste to 
+get away, ran across the Lion's nose. Roused from his nap, the Lion laid his 
+huge paw angrily on the tiny creature to kill her.
+
