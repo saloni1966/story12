4feb3b474be3d014cfcec7b3a72707e42c7c709b
saloni <salonisharma1966@gmail.com>
2021-08-25 05:48:22 +0530

    3rd commit

--- story.js
+++ story.js
@@ -9,3 +9,10 @@ huge paw angrily on the tiny creature to kill her.
 
 The Lion was much amused to think that a Mouse could ever help him.
  But he was generous and finally let the Mouse go.
+
+
+ "Spare me!" begged the poor Mouse. 
+ "Please let me go and some day I will surely repay you."
+
+The Lion was much amused to think that a Mouse could ever help him. 
+But he was generous and finally let the Mouse go.
